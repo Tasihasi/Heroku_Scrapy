96d6f5112d0c502ba684ddba97cf4b5c60abd4a0
Tasihasi <taskovics.dani12@gmail.com>
2023-11-25 22:22:49 +0100

    eeeee

--- app/api_manager/api_route.py
+++ app/api_manager/api_route.py
@@ -124,7 +124,7 @@ def get_proxies():
 
 @proxy_blueprint.route('/get_final_data', methods=['GET'])
 def Get_final_data():
-    path = "./app/heroku_scrapy/Result.xml"
+    path = "./heroku_scrapy/Result.xml"
 
     log_folder_content("app/heroku_scrapy")
 
