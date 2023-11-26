bde9a5ebb336695a9ec1ab2795271b97d4fe4270
Tasihasi <taskovics.dani12@gmail.com>
2023-11-26 04:40:20 +0100

    big update to the arukeres_all.py and increasing the request number

--- app/heroku_scrapy/heroku_scrapy/spiders/arukereso_all.py
+++ app/heroku_scrapy/heroku_scrapy/spiders/arukereso_all.py
@@ -19,6 +19,8 @@ from scrapy.crawler import CrawlerProcess
 from twisted.internet import reactor, threads
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
+from typing import List
+from urllib.parse import urlparse, urlencode, urlunparse,quote, parse_qs
 
 
 import logging
@@ -27,73 +29,16 @@ import logging
 
 logging.info("Spider started")
 
-"""
-def check_proxies(q, valid_proxy_list):
-        while not q.empty():
-            proxy = q.get()
-            try:
-                res = requests.get("https://tablet-pc.arukereso.hu/", 
-                                proxies={"http": proxy, "https": proxy})
-            except:
-                continue
-            if res.status_code == 200:
-                valid_proxy_list.append(proxy)
-"""
-
-def run_scrapy_in_thread():
-    runner = CrawlerRunner()
-
-    # This function will be called in a thread
-    def crawl():
-        d = runner.crawl(FreeProxyListSpider)
-        d.addBoth(lambda _: reactor.stop())
-
-    # Run the crawl function in the reactor thread
-    threads.deferToThread(crawl)
-
-    # Start the reactor
-    reactor.run()
 
 
-def check_proxies(q, valid_proxy_list, num_threads=10):
-    def check_proxy(proxy):
-        logging.info("Currently checking this proxy: %s", proxy)
-        #print("logging in print: ", proxy)
-        try:
-            res = requests.get("https://tablet-pc.arukereso.hu/", 
-                               proxies={"http": proxy, "https": proxy},
-                               timeout=2.5,
-                                 )
-            
-        except Exception as e:
-            print("Exception occurred:", e)
-            return
 
-        logging.info("Response Code for %s: %s", proxy, res.status_code)
-        #print("logging in print: ", res.status_code)
-        if res.status_code == 200:
-            valid_proxy_list.append(proxy)
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
-        # Submit proxy checking tasks to the thread pool
-        while not q.empty():
-            proxy = q.get()
-            executor.submit(check_proxy, proxy)
 
 def Getting_new_proxies():  # Runnin the scrapy 
         # Define the command as a list of strings
-        print()
-        print("--------------------   Getting new proxies ---------------------------")
         logging.info("--------------------   Getting new proxies ---------------------------")
         current_directory = os.getcwd()
         print("Current Working Directory:", current_directory)
-        print()
-
-        # Define the command as a list of strings
-        #command = ['scrapy', 'crawl', 'free_proxy_list', '-O', 'proxies.txt']
-
-        # Run the command
-        #subprocess.run(command)
 
         url = "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all"
         output_file = "proxyes.txt"
@@ -119,76 +64,26 @@ def Getting_new_proxies():  # Runnin the scrapy
             print(f"An error occurred: {e}")
 
 
-
-
-
-def Get_valid_Proxy_list(): # Return with a list of valid proxies or with a false value      
-    logging.info("---- Get_valid_proxy_list arukereso_all.py---")
-    
-    q = queue.Queue()
-    valid_proxy_list = []
-
-    file_name = "./proxyes.txt"
-
-    Getting_new_proxies()
-    j = 0
-        
-    while j < 11 and  not os.path.isfile(file_name):
-        Getting_new_proxies()
-        j+=1
-
-    if os.path.isfile(file_name):
-        # Open the file and read its contents
-        with open(file_name, "r") as f:
-                lines = f.readlines()
-
-        # Check if there are at least 3 lines in the file
-        if len(lines) >= 3:
-            # Remove the first 3 lines
-            del lines[:3]
-
-            # Write the modified content back to the file
-            with open(file_name, "w") as f:
-                    f.writelines(lines)
-        else:
-            # Handle the case where there are fewer than 3 lines in the file
-            print("Not enough lines to remove.")
-
-        with open(file_name, "r") as f:
-            proxies = f.read().split("\n")
-            for p in proxies:
-                q.put(p)
-
-        threads = []
-
-        for _ in range(3):
-            thread = threading.Thread(target=check_proxies, args=(q, valid_proxy_list))
-            thread.start()
-            threads.append(thread)
-
-        for thread in threads:
-            thread.join()
-
-        # Check if you have found 15 valid proxies
-        if len(valid_proxy_list) >= 15:
-            return valid_proxy_list
-
-        return valid_proxy_list
-    else:
-        print("There was no file here!!")
-        return valid_proxy_list
-
-
-
 class ArukeresoSpider(scrapy.Spider):
     name = 'arukereso_all'
     start_urls = ['https://www.arukereso.hu/nyomtato-patron-toner-c3138/']
 
     custom_settings = {
         'DOWNLOAD_DELAY': 0.2,  # add download delay of 1 second
-        'CONCURRENT_REQUESTS': 10,  # Adjust the concurrency level as needed
+        'CONCURRENT_REQUESTS': 50,  # Adjust the concurrency level as needed
+        'CONCURRENT_REQUESTS_PER_DOMAIN' : 3, # for the current limit this must be so high
+        'RETRY_TIMES': 5,  # Number of times to retry a failed request
+        'RETRY_HTTP_CODES': [500, 502, 503, 504, 408, 443],  # HTTP status codes to retry
 
     }
+    def predicting_url(self, url : str) -> List[str]:
+
+        ret_list = [url]
+
+        for i in range(1, 3250):
+            ret_list.append(url + "?start=" + str(i*25))
+        
+        return ret_list
 
     def __init__(self, *args, **kwargs):
         super(ArukeresoSpider, self).__init__(*args, **kwargs)
@@ -196,49 +91,29 @@ class ArukeresoSpider(scrapy.Spider):
         #self.valid_proxies = Get_valid_Proxy_list() #["195.123.8.186:8080"] #
         self.raw_proxy_list = Getting_new_proxies()
         self.proxies_retries = 0
-        #print("----------- Got valid Proxies. ------------------")
+        self.start_urls = self.predicting_url(self.start_urls[0])
+        self.error_urls = []  # List to store URLs that encountered errors
+
         logging.info("----------- Got valid Proxies. ------------------")
-        #logging.info("---  Here is the self.valid_proxy list:  ", self.valid_proxies)
 
-
-        # Start a thread to periodically update the proxy list
-        # it starts a new reactore and breks the code 
-
-        # -------  Temporary closing this line ------ !!!
-        #update_thread = threading.Thread(target=self.update_proxy_list)
-        #update_thread.daemon = True  # This will make the thread exit when the main program exits
-        #update_thread.start()
 
     
-    def update_proxy_list(self):
-        while len(self.valid_proxies) < 10:
-            if len(self.valid_proxies) < 10:
-                new_proxies = Get_valid_Proxy_list()  # Fetch new proxies here
-                self.valid_proxies.extend(new_proxies)
-
-            # Sleep for some time before checking again
-            time.sleep(60)  # Adjust the interval as needed
-
-
+    
 
     def select_proxy(self):
-        # Select a random proxy from the list of valid proxies
-        #print("here is the self.valid proxies: ")
-        #print(self.valid_proxies)
-        #if not self.valid_proxies:
-            #return None
-
-        #return random.choice(self.valid_proxies)
-
         if not self.raw_proxy_list:
-            return None
-        
-        
-        return random.choice(self.raw_proxy_list)
+            return None   
+
+        selected_proxy = random.choice(self.raw_proxy_list)
+
+        while selected_proxy == "-" or not selected_proxy:
+            selected_proxy = random.choice(self.raw_proxy_list)
+
+        return selected_proxy
 
     def check_proxy_status(self, proxy):
         try:
-            response = requests.get("https://tablet-pc.arukereso.hu/", proxies={"http": proxy, "https": proxy}, timeout=10)
+            response = requests.get("https://www.arukereso.hu/nyomtato-patron-toner-c3138/", proxies={"http": proxy, "https": proxy}, timeout=10)
             if response.status_code == 200:
                 return True
         except requests.exceptions.RequestException:
@@ -265,8 +140,9 @@ class ArukeresoSpider(scrapy.Spider):
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}
 
 
+
         request = scrapy.Request(
-            url=response.url,
+            url=(response.url),
             callback=self.parse_link,
             dont_filter=True,
             meta={'proxy': proxy},
@@ -294,34 +170,7 @@ class ArukeresoSpider(scrapy.Spider):
                 else:
                     yield {'name': n, 'price': p.strip(), 'competitor': c}
                     self.blue_product += 1
-
-        next_page = response.css('div.pagination a[data-akpage=">"]::attr(href)').get()
-        if next_page:
-            yield response.follow(next_page, self.parse)
-
-        # Checking if the proxy is still alive
-        if self.check_proxy_status(proxy):
-            # Chossing new proxy if there are proxies
-            if proxy:
-                proxy = self.select_proxy()
-            else:
-                print("--------------- There was no proxies in the Parse Function ------------")
-                return
-        else:
-            # Removing proxy from the list if not responding
-            # Excluding the value error possibility
-            pass
-            #if proxy in self.valid_proxies:
-               #self.valid_proxies.remove(proxy)
             
-        
-        # Check if you need to get new proxies (e.g., fewer than 10 valid proxies)
-        # Temporarly removing this codition !!!!! --------N
-        if len(self.valid_proxies) < 5:
-            self.valid_proxies = Get_valid_Proxy_list()
-
-        logging.info("-------------------------- Reached the bottom of parse function in arukereso_all.py ----------------------")
-
 
     def parse_link(self, response):
         prices = response.css('span[itemprop="price"]::text').getall()
@@ -335,12 +184,8 @@ class ArukeresoSpider(scrapy.Spider):
         prices = [p.replace(' ', '').replace('Ft', '') for p in prices]
         availabilities = [a.lower() for a in availabilities]
 
-        # iterate through the extracted data and yield a dictionary for each item
-        lowest_data = None
-        lowest_data_price = -1
-
-
         i = 0
+        data = {}
 
         for price, availability, competitor in zip(prices, availabilities, competitors):
             i+=1
@@ -350,56 +195,18 @@ class ArukeresoSpider(scrapy.Spider):
                 'competitor': competitor,
                 'product_name': product_name  # use the extracted product name here
             }
-
-            if not lowest_data:
-                lowest_data = data
-                lowest_data_price = int(data['price'])
-
-            elif lowest_data_price > int(data['price']):
-                lowest_data = data
-                lowest_data_price = int(data['price'])
-
-            if i == 10:
-                break
         
-        logging.info("Here is the data i think: %s", lowest_data)
-        yield lowest_data
+        yield data
 
-    def csvFile_Reader(self):
-        pass
+        if response.status != 200:
+            self.error_urls.append(response.url)
 
-class FreeProxyListSpider(scrapy.Spider):
-    name = 'free_proxy_list'
-    start_urls = ['https://www.sslproxies.org/']
+    def restart_parsing(self):
+        # Function to replicate initial parsing behavior
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
-    def parse(self, response):
-        # Check if the "Get raw list" link is present in the page source
-        raw_list_link = response.css('a[title="Get raw list"][data-toggle="modal"]::attr(href)').get()
-
-        if raw_list_link:
-            # Extract the link
-            raw_list_url = response.urljoin(raw_list_link)
-            yield {
-                'raw_list_url': raw_list_url,
-            }
-            
-            # Now, let's extract the content of the <textarea>
-            textarea_content = response.css('textarea.form-control[readonly="readonly"]::text').get()
-            if textarea_content:
-                # You can extract the desired content here
-                yield {
-                    'textarea_content': textarea_content.strip(),
-                }
-                
-                # Save the extracted content to a text file
-                self.save_to_file(textarea_content.strip())
-            else:
-                self.logger.warning('Textarea content not found on the page.')
-        else:
-            self.logger.warning('"Get raw list" link not found on the page.')
-
-    def save_to_file(self, content):
-        with open('proxies.txt', 'w') as file:
-            file.write(content)
-
-
+    def closed(self, reason):
+        if self.error_urls:
+            self.start_urls = self.error_urls
+            yield from self.restart_parsing()
--- app/main.py
+++ app/main.py
@@ -1,5 +1,36 @@
 from flask import Flask
 from .api_manager.api_route import api, proxy_blueprint
+import requests
+import time
+from datetime import datetime, timedelta
+
+
+def send_request():
+    url = "https://herokuscrapy-8d468df2dace.herokuapp.com/get_data"
+
+    response = requests.get(url)
+
+    print("Status Code:", response.status_code)
+    print("Response Body:", response.text)
+
+def run_daily_job():
+    # Set the target time for the daily job (9:00 AM)
+    target_time = datetime.combine(datetime.today(), datetime.min.time()) + timedelta(hours=9)
+
+    while True:
+        # Calculate the time until the target time
+        current_time = datetime.now()
+        time_diff = (target_time - current_time).total_seconds()
+
+        if time_diff <= 0:
+            # If current time is past the target time, execute the job
+            send_request()
+            # Schedule the next run for the next day
+            target_time += timedelta(days=1)
+        else:
+            # Sleep until the next check (every minute in this case)
+            time.sleep(60)
+
 
 
 # Create a Flask application instance
@@ -7,18 +38,8 @@ app = Flask(__name__)
 
 # Register the API blueprint
 app.register_blueprint(api)  # Register the 'api' Blueprint
-
 app.register_blueprint(proxy_blueprint)
-
-# Define a route and view function
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-
 
 # Define a main function to run the app
 if __name__ == '__main__':
-    app.run()
-
-
+    app.run(debug=True, threaded=True)
