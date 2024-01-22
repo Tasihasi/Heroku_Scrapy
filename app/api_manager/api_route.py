15405bd5fa6c6287070deafbbb2d4835dc17ade0
Tasihasi <taskovics.dani12@gmail.com>
2024-01-22 19:25:55 +0100

    adding the xml to the <items> tag 5.2

--- app/api_manager/api_route.py
+++ app/api_manager/api_route.py
@@ -191,7 +191,7 @@ def Get_final_data():
         data = json.loads(line)
                 
         # Check if all required attributes are present
-        if all(attr in data for attr in ['price', 'availability', 'competitor']):
+        if all(attr in data for attr in ['price', 'availability', 'competitor', 'product_name']) and data['availability'] != '\n':
         # Create an XML element for each JSON object
             #item_element = ET.SubElement(items_element, "item")  # Use items_element as the parent
             
