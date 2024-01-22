c58af64863f72bfad70c5cac4f4c93c00aa754f4
Tasihasi <taskovics.dani12@gmail.com>
2024-01-22 23:18:45 +0100

    adding the xml to the <items> tag 5.12.1

--- app/api_manager/api_route.py
+++ app/api_manager/api_route.py
@@ -50,6 +50,7 @@ def jsonL_to_xml(jsonl_file, xml_file):
 
             # Add sub-elements for each key-value pair in the JSON object
             for key, value in json_data.items():
+                logging.info(f"----- the key:  {key}   -----  value :   {value}--------")
                 if value is not None and value != "" and value != "\n":
                     sub_element = ET.SubElement(element, key)
                     sub_element.text = str(value)
