51d5d42cb426661c56a84102ab526cce44f17add
Tasihasi <taskovics.dani12@gmail.com>
2024-03-04 11:57:52 +0100

    trying to get file by my self

--- app/api_manager/google_drive/google_adrive_apiendpoint.py
+++ app/api_manager/google_drive/google_adrive_apiendpoint.py
@@ -49,53 +49,9 @@ def get_file():
     logging.info(f"--- logging the request.args dictorany:  {request.args}")
 
 
-    try:
-
-        logging.info("entered try branch")
-        logging.info(f"--- logging the request.args dictorany:  {request.args}")
-        logging.info(f"--- logging the request.args dictorany:  {request.args}")
-
-        file_id = request.args.get('file_id')  # Get the file ID from the request parameters
-        logging.info("Cant get file id from URL")
-        if not file_id:
-            return jsonify({'error': 'File ID is required'}), 400
-
-        # Get authenticated Drive API service
-        drive_service = Get_drive_service()
-
-        logging.info("Successfully authenticated with Google Drive API in get file endpoint")
-
-        # Call Drive API to get file metadata
-        file_metadata = drive_service.files().get(fileId=file_id).execute()
-
-        # Download the file
-        request = drive_service.files().get_media(fileId=file_id)
-        file_bytes = BytesIO()
-        downloader = MediaIoBaseDownload(file_bytes, request)
-        done = False
-        while done is False:
-            status, done = downloader.next_chunk()
-
-        # Set content type based on file type
-        content_type = file_metadata['mimeType']
-
-        # Return the file as a response
-        file_bytes.seek(0)
-        return send_file(file_bytes, mimetype=content_type, as_attachment=True, attachment_filename=file_metadata['name'])
     
-    except Exception as e:
-        error_details = getattr(e, 'resp', {}).get('error', {})
-        error_reason = error_details.get('message', 'Unknown error')
-        if isinstance(e, requests.exceptions.HTTPError) and e.response.status_code == 404:
-            logging.error("File not found: {}".format(error_reason))
-            return jsonify({'error': 'File not found'}), 404
-        else:
-            logging.error("Error occurred while retrieving file: {}".format(error_reason))
-            if hasattr(e, 'response') and hasattr(e.response, 'json'):
-                logging.error("Google Drive Response: {}".format(e.response.json()))
-            else:
-                logging.error("Google Drive Response: No response available")
-            return jsonify({'error': 'Error occurred while retrieving file'}), 500
+    
+    
 
 @google_drive_api.route('/create_file', methods=['POST'])
 def create_file():
