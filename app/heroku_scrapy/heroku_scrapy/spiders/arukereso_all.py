4314e07d3d557665b2d48261a1ad751fa712b170
Tasihasi <taskovics.dani12@gmail.com>
2024-05-12 13:35:49 +0200

    correcting log in the closing statement

--- app/heroku_scrapy/heroku_scrapy/spiders/arukereso_all.py
+++ app/heroku_scrapy/heroku_scrapy/spiders/arukereso_all.py
@@ -358,7 +358,7 @@ class ArukeresoSpider(scrapy.Spider):
         logging.critical(f" -------  Time took parsing the data : {self.parsing_time[0]}  -------")
         logging.critical(f" -------  Time took parsing the link : {self.parsing_time[1]}  -------")
         logging.critical(f" -------  Time took parsing SUM : {sum(self.parsing_time)}  -------")
-        logging.critical(f" -------  Time took run the spider : {(datetime.now() - self.crawling_time)}  -------")
+        logging.critical(f" -------  Time took run the spider : {(datetime.now() - self.crawling_time).total_seconds()}  -------")
 
 
         #self.push_to_google_drive("output.jsonl")
